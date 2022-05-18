dd96984699d68ea29e3287f78cdb2a1b7a6c5e6d
marianorapa <marianorapaport@gmail.com>
2022-05-18 00:11:45 -0300

    save positions in 06

---
+++ TP04/06/06_indexer.py
@@ -0,0 +1,124 @@
+import math
+from memory_indexer import Indexer
+import struct
+import sys
+
+class FrequencyIndexer:
+
+    def __init__(self, index_output_path = "05_index.bin", vocabulary_output_path = "05_vocab.bin", doc_norms_output_path = "05_doc_norms.bin", positions_output_path = "05_positions.bin"):
+        self.base_indexer = Indexer()
+        self.posting_format = "IIH"
+        self.posting_entry_size = 6
+        self.index_output_path = index_output_path
+        self.vocabulary_output_path = vocabulary_output_path        
+        self.doc_norms_output_path = doc_norms_output_path
+        self.positions_output_path = positions_output_path
+        self.vocabulary = {}        
+        
+        self.VOCAB_TERM_LENGTH = 50         # longer terms will be truncated in vocab
+    
+    def get_max_term_length(self):
+        return self.base_indexer.get_max_term_length()
+
+    def index_dir(self, dir):
+        self.index, self.doc_vectors, self.docs_total_terms = self.base_indexer.index_dir(dir)
+        self.__save_index__()        
+        #self.__save_vocabulary__()
+        #self.__calculate_and_save_docs_norm_by_tf__()
+    
+    def get_index_size(self):
+        return len(self.index.keys())
+
+    def get_vocab_size(self):
+        return len(self.vocabulary.keys())
+
+    def __save_index__(self):          
+        index_pointer = 0
+        positions_pointer = 0
+        with open(self.index_output_path, "wb") as index_file:
+            with open(self.positions_output_path, "wb") as positions_file:
+                for term in self.index.keys():
+                    docs_positions = self.index[term]                    
+                    index_output_values = []    # [doc_id, pointer_position, tf]
+                    for doc_id in docs_positions:
+                        term_doc_positions = docs_positions[doc_id]                        
+                        tf = len(term_doc_positions)
+                        positions_format = "H" * tf
+                        packed_positions = struct.pack(positions_format, *term_doc_positions)                        
+                        bytes_written = positions_file.write(packed_positions)
+                        # Guardar puntero de las posiciones del documento
+                        index_output_values.append(doc_id)
+                        index_output_values.append(positions_pointer)
+                        index_output_values.append(tf)
+
+                        # Incrementar puntero del archivo de posiciones
+                        positions_pointer += bytes_written
+
+                    df = len(docs_positions.keys())
+                    self.vocabulary[term] = [df, index_pointer]
+                    index_output_format = df * self.posting_format                
+                    
+                    packed_postings = struct.pack(index_output_format, *index_output_values)
+                    bytes_written = index_file.write(packed_postings)
+                    index_pointer += bytes_written
+    
+    def __save_vocabulary__(self):
+        #max_term_length = self.base_indexer.get_max_term_length()        
+        max_term_length = self.VOCAB_TERM_LENGTH
+        with open(self.vocabulary_output_path, "wb") as file:
+            for term in self.vocabulary.keys():
+                try:
+                    df = self.vocabulary[term][0]
+                    pointer = self.vocabulary[term][1]                
+                                        
+                    output_format = "IH"                
+                    packed_values = struct.pack(output_format, pointer, df)                    
+                    encoded_term = term.encode('utf-8')
+                    
+                    if len(encoded_term) > max_term_length:
+                        encoded_term = encoded_term[:max_term_length]
+                    else:
+                        # Fill with spaces
+                        encoded_term = encoded_term + (" " * (max_term_length - len(encoded_term))).encode("utf-8")                        
+                    written_bytes = file.write(encoded_term)                    
+                    written_bytes += file.write(packed_values)
+                    if written_bytes != (self.VOCAB_TERM_LENGTH + struct.calcsize(output_format)):
+                        print(f"Error: written bytes {written_bytes} defer from expected size {self.VOCAB_TERM_LENGTH + struct.calcsize(output_format)} for term {term} with length {len(term)} and entry {struct.calcsize(output_format)}")
+                except Exception as e:
+                    print(f"Couldn't save term {term}: {e}")
+        file.close()
+       
+
+    def __calculate_and_save_docs_norm_by_tf__(self):           
+        doc_norms = {}
+        for doc_id in self.doc_vectors.keys():
+            doc_vector = self.doc_vectors[doc_id]
+            sum = 0
+            for term in doc_vector.keys():
+                tf = doc_vector[term]
+                idf = math.log(len(self.vocabulary) / self.vocabulary[term][0])
+                sum += pow(tf*idf, 2)
+            doc_norms[doc_id] = math.sqrt(sum)
+        with open(self.doc_norms_output_path, "wb") as file:
+            for doc_id in doc_norms.keys():
+                try:
+                    norm = doc_norms[doc_id]                                        
+                    output_format = "If"                
+                    packed_values = struct.pack(output_format, doc_id, norm)                    
+                    file.write(packed_values)
+                except:
+                    print(f"Couldn't save term {doc_id}'s norm")                
+        file.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Es necesario pasar como argumento un path a un directorio')
+        sys.exit(0)
+    indexer = FrequencyIndexer()
+    indexer.index_dir(sys.argv[1])
+        
+    index_size = indexer.get_index_size()
+    print(f"Index size (in terms) is: {index_size}")
+    vocab_size = indexer.get_vocab_size()
+    if index_size != vocab_size:
+        print("Something is wrong with index and vocab size")
---
+++ TP04/06/memory_indexer.py
@@ -0,0 +1,108 @@
+from calendar import c
+from pydoc import doc
+import pathlib
+import pickle as pickle
+from tokenizer import Tokenizer
+
+# Generates an index (dict) with each term as a key. Values are the posting lists with [doc_id, TF]
+
+class Indexer:
+
+    def __init__(self):
+
+        self.MIN_LENGTH = 3
+        self.MAX_LENGTH = 30
+
+        self.tokenizer = Tokenizer(self.MIN_LENGTH, self.MAX_LENGTH, proper_name_splitting=True)
+        self.index = {}
+
+        self.palabras_vacias = []
+
+        self.total_docs              = 0
+        self.total_tokens            = 0
+        self.total_terms             = 0
+        self.total_terms_length      = 0
+        
+        self.max_term_length = 0
+
+
+    def get_max_term_length(self):
+        return self.max_term_length
+
+    def __read_palabras_vacias(self, path):
+        output = []
+        with open(path, "r") as f: 
+            for line in f.readlines():
+                stop_words = line.split(",")
+                output.append(stop_words)
+        return [item for sublist in output for item in sublist]
+
+    def __search_files(self, dir, files = [], recursive = True):
+        path = pathlib.Path(dir)        
+        for item in path.iterdir():            
+            if (item.is_file()):                
+                files.append(item.absolute())
+            else:                
+                if (item.is_dir() and recursive):
+                    self.__search_files(item, files)            
+
+    def __index_doc(self, doc_id, doc, index, docs_terms):        
+        try:
+            with open(doc, "r", encoding="utf-8") as f:                
+                
+                tokens = self.tokenizer.get_tokens_with_frequency(f.read())
+                docs_terms[doc_id] = [0, doc.stat().st_size]
+                for token in tokens.keys():
+                    if token not in self.palabras_vacias:
+                        if token not in index:
+                            index[token] = {}
+                            if len(token) > self.max_term_length:
+                                self.max_term_length = len(token)
+                        if doc_id not in index[token]:
+                            index[token][doc_id] = []    
+                        
+                        positions = tokens[token][1]
+                        
+                        index[token][doc_id] = positions
+
+                        docs_terms[doc_id][0] += 1   # Este documento tiene un término más
+                return tokens
+        except Exception as e:
+            print(f"Error indexing doc {doc}: {e}")
+
+    def get_index(self):
+        return self.index
+
+    def index_dir(self, directory):
+        self.dir = directory
+        files = []
+        self.__search_files(directory, files)        
+        index, doc_vectors, docs_total_terms = self.__index_files(files)
+        self.index = index
+        return self.index, doc_vectors, docs_total_terms
+
+    # Indexing calls
+    def __index_files(self, files):        
+        index = {}
+        doc_terms = {}
+        doc_id = 1
+        doc_vectors = {}
+        with open("doc_ids.csv", mode="w", encoding="utf-8") as doc_ids_file:
+            for file in files:
+                current_file = pathlib.Path(file)
+                tokens = self.__index_doc(doc_id, current_file, index, doc_terms)
+                doc_vectors[doc_id] = tokens
+                doc_ids_file.write(f"{current_file.name},{doc_id}\n")
+                doc_id += 1            
+            self.total_docs = doc_id - 1
+        return index, doc_vectors, doc_terms
+
+    def get_terms_from_query(self, query):
+        return self.tokenizer.get_tokens_with_frequency(query)
+
+    # ----- Saving
+
+    def __save_index(self, dict):
+        path = self.dir + "index.pcl"
+        with open(path, "wb") as f:                
+            pickle.dump(dict, f)
